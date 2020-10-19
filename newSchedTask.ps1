2d6530b5b84b82b648d964df8a63eb081edc44b1
svc.gitSync <jeremy.isaac.brown+WinHiEd@gmail.com>
2020-10-19 14:40:52 -0700

    Second Commit

---
+++ newSchedTask.ps1
@@ -0,0 +1,30 @@
+﻿$TaskActionSplat = @{
+    Execute = 'PowerShell.exe'
+    Argument = '-file C:\scripts\GitCronSync.ps1 -WindowStyle Hidden'
+}
+$TaskAction = New-ScheduledTaskAction @TaskActionSplat
+
+$TaskTriggerSplat = @{
+    At = '1am'
+    RepetitionInterval = New-TimeSpan -Minutes 60
+    RepetitionDuration = New-TimeSpan -Minutes 60
+}
+$TaskTrigger =  New-ScheduledTaskTrigger -Once @TaskTriggerSplat
+
+$TaskSettingsSplat = @{
+    RestartCount = 3
+    RestartInterval = New-TimeSpan -Minutes 5
+    ExecutionTimeLimit = New-TimeSpan -Minutes 60
+}
+$TaskSettings = New-ScheduledTaskSettingsSet @TaskSettingsSplat
+
+$RegisterTaskSplat = @{
+    Action = $TaskAction
+    Trigger = $TaskTrigger
+    Settings = $TaskSettings
+    TaskName = 'GitCronSync'
+    Description = 'Commits files from C:\GitCronSync to GitHub repo'
+    User = 'GitCronSync'
+    Password = 'P@ssw0rd'
+}
+Register-ScheduledTask @RegisterTaskSplat
